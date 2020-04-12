fff197288f41cc36e563a51cedc8cbb975105a96
Jack Cooper <jackcooper1245@gmail.com>
2020-04-12 09:59:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
                        :three_pieces_owned=>100,
                        :four_pieces_owned=>200
                       },
-              :names => {}
+              :names => {}}
 end
 
 def monopoly_with_fourth_tier
